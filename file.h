c59041a09f258866a81a398032f04bdaeef3475e
Chandrakant <vyavaharecg17.it@coep.ac.in>
2018-10-14 06:28:59 +0530

    *added some enum definations *added Fclose *added some comments *declared array of file

--- file.h
+++ file.h
@@ -1,7 +1,15 @@
-typedef struct file {
-	int flags;
-	int mode;
-	int err;
+typedef struct _file {
+	int fd; //file descreptor
+	int flags; // mode of opening of file
 }file;
-file *Fopen(char const *filepath, char const *mode);
+#define MAX_SIZE 20
+enum _flags {
+	_READ = 01;
+	_WRITE = 02;
+	_EOF = 03;
+	_ERR = 04;
+};
 
+extern file arrfile[MAX_FILE]; // maximum files that can be opened
+file *Fopen(char *filepath, char *mode);
+file Fclose(file *fp);
